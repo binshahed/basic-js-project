cf367d475abe3a3a4f7fe01ac3979875a46e9eea
Mohammad Bin Shahed <mdbinshahed5@gmail.com>
2021-05-08 16:11:23 +0600

    Digital Clock

---
+++ digita-clock-tutorial/app.js
@@ -0,0 +1,28 @@
+function digitalClock() {
+  const timeFormate = document.getElementById("timeFormate");
+
+  let date = new Date();
+  let hours = date.getHours();
+
+  if (hours > 12) {
+    timeFormate.innerHTML = "pm";
+    hours = hours - 12;
+  } else if (hours == 0) {
+    hours = 12;
+  } else {
+    timeFormate.innerHTML = "am";
+  }
+
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document.getElementById("time").innerHTML =
+    hours + ":" + minutes + ":" + seconds;
+  setInterval(digitalClock, 1000);
+}
+
+digitalClock();
